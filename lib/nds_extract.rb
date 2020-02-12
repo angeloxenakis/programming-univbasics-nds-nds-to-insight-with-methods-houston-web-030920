2c931a83c356d4389d87b263120bcd31ffa38e0d
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-12 03:08:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,27 +4,27 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
-  gross_total = 0 
-  inner_length = directors_database[director_data][:movies].length
-  inner_index = 0 
-  while inner_index < inner_length do
-    gross_total += directors_database[director_data][:movies][inner_index]
-    inner_index += 1
-  end
-  gross_total
-end
-
-# def gross_for_director(nds, row_index, column_index)
-#   director_total = 0
-#   inner_len = directors_database[director_data][:movies].length
-#   inner_index = 0
-#   while inner_index < inner_len do
-#     director_total += nds[row_index][column_index][inner_index][:price]
+# def gross_for_director(director_data)
+#   gross_total = 0 
+#   inner_length = directors_database[director_data][:movies].length
+#   inner_index = 0 
+#   while inner_index < inner_length do
+#     gross_total += directors_database[director_data][:movies][inner_index]
 #     inner_index += 1
 #   end
-#   director_total
+#   gross_total
 # end
+
+def gross_for_director(nds, row_index, column_index)
+  director_total = 0
+  inner_len = directors_database[director_data][:movies].length
+  inner_index = 0
+  while inner_index < inner_len do
+    director_total += nds[row_index][column_index][inner_index][:price]
+    inner_index += 1
+  end
+  director_total
+end
 
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
