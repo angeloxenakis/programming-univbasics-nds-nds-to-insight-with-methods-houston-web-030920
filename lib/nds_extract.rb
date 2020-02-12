630f7cc67ad0636d7c490a55cc23c85afe2edf35
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-12 03:13:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,8 +33,16 @@ end
 def directors_totals(nds)
   result = {
   }
-  director_index = 0 
-  while director_index < directors_database.length do
-    gross_for_director()
-    
+  directors_index = 0 
+  while directors_index < directors_database.length do
+    movies_index = 0
+    gross_total = 0
+    while movies_index < directors_database[directors_index][:movies].length do
+      gross_total = gross_total + directors_database[directors_index][:movies][movies_index][:worldwide_gross]
+      movies_index += 1
+    end
+    result[directors_database[directors_index][:name]] = gross_total
+    directors_index += 1
+  end
+  pp result
 end
