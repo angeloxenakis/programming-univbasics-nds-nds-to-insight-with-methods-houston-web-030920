830f1bf738d40fc68c46cac118b4e552b1f07d1b
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-12 02:19:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+binding.pry
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
